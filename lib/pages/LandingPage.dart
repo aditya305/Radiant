7fbc20b9dc4da5b58b41a6b75a8062e5832a7bab
Aditya Sutar <41361167+aditya305@users.noreply.github.com>
2020-06-29 15:15:00 +0530

    temp update

--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:radiant/utils/app_info.dart';
-import 'package:radiant/utils/app_theme.dart';
-import 'package:radiant/widget/custoAppBar.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-
-    ],);
+    return Column(
+      children: [
+         AppBar(
+          leading: Text(
+            AppInfo.appName,
+            style: GoogleFonts.nunito(
+              fontSize: 27.0,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
-
-
